d21a4fe0884c23ca9c51540502db6a3b9bfdad65
Farzad FARID <farzad.farid@kapten.com>
2020-04-22 22:26:28 +0200

    Basic struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+struct Foo {
+    quax: i32,
+    baz: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = Foo {
+        quax: 10,
+        baz: String::new(),
+    };
+
+    println!("Foo: quax: {}, baz: {}", a.quax, a.baz);
 }
